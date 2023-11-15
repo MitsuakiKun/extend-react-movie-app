23cc441cf63c0bd88264eeb73509811a35abebc9
Mitsuaki Okabe <mitsu3368_1024@icloud.com>
2023-11-15 13:28:22 +0000

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -32,18 +32,50 @@ const LoginPage = () => {
   
     return (
       <>
-      {user ? (
-        <Navigate to={`/`} />
-      ) : (
-        <>
-          <div>
-            <h1>Login Page</h1>
-            <button onClick={handleLogin}>Login with @mail.wit.ie Google Account</button>
-          </div>
-        </>
-      )}
+        <div style={styles.container}>
+          <img
+              src="http://devops.witdemo.net/logo.jpg"
+              alt="Logo"
+              style={styles.logo}
+          />
+          <h1 style={styles.heading}>Login Page</h1>
+          <button style={styles.button} onClick={handleLogin}>
+            Login with @mail.wit.ie Google Account
+          </button>
+
+        </div>
       </>
-  );
-};
+    );
+  };
+  
+  const styles = {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '70vh',
+      fontFamily: 'Arial, sans-serif'
+    },
+    heading: {
+      fontSize: '2rem',
+      marginBottom: '20px',
+      color: '#1e364d',
+    },
+    button: {
+      padding: '10px 20px',
+      fontSize: '1rem',
+      backgroundColor: '#4caf50',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+    },
+    logo: {
+      width: '370px', 
+      height: '204px', 
+      marginBottom: '20px',
+    },
+  };
   
   export default LoginPage;
